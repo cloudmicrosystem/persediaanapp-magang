79e0ea027d08e2392545bd6efb9fdd49122c6448
Yovieaji13 <yoviehaditamaw@gmail.com>
2022-01-06 11:05:14 +0700

    commit_dbinbis

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>@yield('title','AdminLTE 3 | Dashboard')</title>
+  <title>@yield('title','Database Persediaan')</title>
 
   {{-- CSS Class Content --}}
   @stack('custom-css')
