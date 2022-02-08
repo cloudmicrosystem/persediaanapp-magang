fc5cd43a0a345da039154154dd8b72b2510931a4
Yovieaji13 <yoviehaditamaw@gmail.com>
2022-02-08 19:33:23 +0700

    template_login_register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,10 +1,16 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <p class="d-flex justify-content-center mb-4">
-                <img src="/logo/logo1.jpg" alt="logo" width="200" >
-            </p>
-        </x-slot>
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Login</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="{{ asset('') }}assets/plugins/login/style.css">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700,800&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="cont">
 
         <!-- Session Status -->
         <x-auth-session-status class="mb-4" :status="session('status')" />
@@ -12,49 +18,128 @@
         <!-- Validation Errors -->
         <x-auth-validation-errors class="mb-4" :errors="$errors" />
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
-
-            <!-- Email Address -->
-            <div>
-                <x-label for="email" :value="__('Email')" />
-
-                <x-input id="email" class="" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
-
-            <!-- Password -->
-            <div class="mt-4">
-                <x-label for="password" :value="__('Password')" />
-
-                <x-input id="password" class=""
-                                type="password"
-                                name="password"
-                                required autocomplete="current-password" />
-            </div>
-
-            <!-- Remember Me -->
-            <div class="mt-3 form-check">
-                <input id="remember_me" type="checkbox" class="form-check-input" name="remember">
-                <label for="remember_me" class="form-check-label text-sm">
-                    {{ __('Remember me') }}
+        <div class="form sign-in">
+            <h2>Sign In</h2>
+            <form method="POST" action="{{ route('login') }}">
+                @csrf
+                <label>
+                    <x-label for="email" :value="__('Email')" />
+                    <x-input id="email" class="" type="email" name="email" :value="old('email')" />
                 </label>
-            </div>
+                <label>
+                    <x-label for="password" :value="__('Password')" />
 
+                    <x-input id="password" class="" type="password" name="password" required
+                        autocomplete="current-password" />
+                </label>
 
-            <div class="d-flex justify-content-end mt-4">
-                <a class="text-muted" href="{{ route('register') }}" style="margin-right: 15px; margin-top: 15px;">
-                    {{ __('Register') }}
-                </a>
-                @if (Route::has('password.request'))
-                    <a class="text-muted" href="{{ route('password.request') }}" style="margin-right: 15px; margin-top: 15px;">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
-
+                <br>
                 <x-button class="ml-3">
                     {{ __('Log in') }}
                 </x-button>
+
+                <p class="forgot-pass">
+                    @if (Route::has('password.request'))
+                        <a class="text-muted1" href="{{ route('password.request') }}"
+                            style="margin-right: 15px; margin-top: 15px;">
+                            {{ __('Forgot your password?') }}
+                        </a>
+                    @endif
+                </p>
+            </form>
+            <div class="social-media">
+                <ul>
+                    <li><img src="logo/facebook.png"></li>
+                    <li><img src="logo/twitter.png"></li>
+                    <li><img src="logo/linkedin.png"></li>
+                    <li><img src="logo/instagram.png"></li>
+                </ul>
             </div>
-        </form>
-    </x-auth-card>
-</x-guest-layout>
+        </div>
+
+        <div class="sub-cont">
+            <div class="img">
+                <div class="img-text m-up">
+                    <h2>New here?</h2>
+                    <p>Sign up and discover great amount of new opportunities!</p>
+                </div>
+                <div class="img-text m-in">
+                    <h2>One of us?</h2>
+                    <p>If you already has an account, just sign in. We've missed you!</p>
+                </div>
+                <div class="img-btn ">
+                    <span class="m-up">Sign up</span>
+                    <span class="m-in">Sign In</span>
+                </div>
+            </div>
+            <div class="form sign-up">
+
+                <!-- Validation Errors -->
+                <x-auth-validation-errors class="mb-4" :errors="$errors" />
+
+                <form method="POST" action="{{ route('register') }}" style="overflow:auto">
+                    @csrf
+
+                    <h2>Sign Up</h2>
+                    <div >
+                        <div class="menu">
+                            <label>
+                                <x-label for="name" :value="__('Nama')" />
+
+                                <x-input id="name" class="" type="text" name="nama" :value="old('name')"
+                                    required autofocus />
+                            </label>
+                            <label>
+                                <div class="mt-4">
+                                    <x-label for="alamat" :value="__('Alamat')" />
+
+                                    <x-input id="alamat" class="" type="text" name="alamat"
+                                        :value="old('alamat')" required />
+                                </div>
+                            </label>
+                            <label>
+                                <div class="mt-4">
+                                    <x-label for="no_hp" :value="__('No Hp')" />
+
+                                    <x-input id="no_hp" class="" type="number" name="no_hp"
+                                        :value="old('no_hp')" required />
+                                </div>
+                            </label>
+                            <label>
+                                <div class="mt-4">
+                                    <x-label for="email" :value="__('Email')" />
+
+                                    <x-input id="email" class="" type="email" name="email"
+                                        :value="old('email')" required />
+                                </div>
+                            </label>
+                            <label>
+                                <div class="mt-4">
+                                    <x-label for="password" :value="__('Password')" />
+
+                                    <x-input id="password" class="" type="password" name="password"
+                                        required autocomplete="new-password" />
+                                </div>
+                            </label>
+                            <label>
+                                <div class="mt-4">
+                                    <x-label for="password_confirmation" :value="__('Confirm Password')" />
+
+                                    <x-input id="password_confirmation" class="" type="password"
+                                        name="password_confirmation" required />
+                                </div>
+                            </label>
+                            <br>
+                            <x-button class="ml-4">
+                                {{ __('Register') }}
+                            </x-button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <script type="text/javascript" src="{{ asset('') }}assets/plugins/login/script.js"></script>
+</body>
+
+</html>
