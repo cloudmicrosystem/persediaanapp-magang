f4f897474068fefa562f65423919596aaa826025
dewa-wildan <wildanaryakusuma123@gmail.com>
2022-03-14 12:49:22 +0700

    commit_footer

---
+++ resources/views/frontend/halcust/refund.blade.php
@@ -0,0 +1,101 @@
+<div id="wrapper">
+    <div id="page" class="container py-3 py-md-4">
+        <div class="row">
+            <div class="col-12">
+                <h2 class="title">Refund Policy</h2>
+            </div>
+            <div class="col-12 page-content text-light">
+                <div class="tab-link">
+                    <a data-id="id" class="active">BAHASA INDONESIA</a>
+                    <a data-id="en">ENGLISH</a>
+                </div>
+                <div class="tab-contents">
+                    <div class="tab-content" data-id="id">
+                        <p><strong>KEBIJAKAN PENGEMBALIAN DANA</strong></p>
+                        <ul>
+                            <li>Barang harus dikembalikan dalam waktu 5 hari setelah barang diterima.</li>
+                            <li>Barang harus memiliki tag asli seperti yang disertakan saat penerimaan.</li>
+                            <li>Kami berhak untuk menolak pengembalian dana atau penukaran apabila barang tersebut telah
+                                berubahan bentuk, terjadi kerusakan, dicuci, atau ketidaklengkapan tag asli yang
+                                disertakan.</li>
+                            <li>Kami tidak menjamin penukaran atau pengembalian dana untuk barang yang tidak memenuhi
+                                kriteria pengembalian. Jika anda menerima produk yang mengalami kerusakan atau cacat
+                                pada saat menerima pengiriman silahkan memberikan pemberitahuan terlebih dahulu dalam
+                                waktu 2 hari setelah anda menerima produk melalui email website@maternaldisaster.com
+                                sebelum mengirim barang kembali.</li>
+                            <li>Anda dapat menggunakan jasa pengiriman lain untuk pengembalian barang. Untuk memastikan
+                                kami menerima barang yang anda kirim silahkan meminta nomor pelacakan dari pengirim dan
+                                konfirmasikan kepada pihak kami melalui email website@maternaldisaster.com beserta nomor
+                                KONFIRMASI PEMBAYARAN anda.</li>
+                            <li>Kemas kembali barang pengembalian anda dengan aman.</li>
+                            <li>Kirim barang pengembalian anda ke alamat : Maternal Disaster Jl Wira Angun Angun No 4,
+                                Bandung.</li>
+                            <li>Barang baru akan di kirim setelah kami menerima barang penukaran anda dan mengkonfirmasi
+                                pengembalian atau penukaran anda.</li>
+                            <li>Kami tidak bertanggungjawab atas kehilangan barang pengembalian saat pengiriman kembali.
+                            </li>
+                            <li>Biaya pengiriman awal tidak dapat dikembalikan.</li>
+                        </ul>
+                        <p><strong>KEBIJAKAN PENUKARAN BARANG</strong></p>
+                        <ol>
+                            <li>Kami hanya dapat menukar barang untuk barang yang terapat kerusakan atau cacat, tidak
+                                menerima penukaran size.</li>
+                            <li>Apabila barang yang bersangkutan telah habis, dana akan dikembalikan penuh dikurangi
+                                biaya pengiriman awal setelah kami menerima pengembalian barang anda.</li>
+                            <li>Kami tidak bertanggungjawab atas kehilangan barang pengembalian saat pengiriman kembali.
+                            </li>
+                        </ol>
+                    </div>
+                    <div class="tab-content" data-id="en">
+                        <p><b>REFUND POLICY</b></p>
+                        <ul>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Product must be returned
+                                    within 5 days of receipt.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Product must have the original
+                                    tags as included at the time of receipt.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Maternal is eligible to refuse
+                                    refund or exchange request when the product has been deformed, damaged, washed, or
+                                    incomplete without the original tag included.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Maternal does not guarantee
+                                    exchange or refunds request for items that do not meet the criteria of return
+                                    policy. </span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">In the case that the product
+                                    is damaged or defective upon receipt of delivery, please provide advance notice
+                                    within 2 days of receipt via email website@maternaldisaster.com</span><span
+                                    style="font-weight: 400;"> before sending it back.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Any shipping services can be
+                                </span>used<span style="font-weight: 400;"> to return the product. Please ensure that we
+                                    receive the product you sent by providing the tracking number and kindly confirm via
+                                    email website@maternaldisaster.com</span><span style="font-weight: 400;"> along with
+                                    your PAYMENT CONFIRMATION number.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Please repack the returned
+                                    product safely.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Kindly send back the product
+                                    to: Maternal Disaster &#8211; Jl. Jl Wira Angun Angun No 4, Bandung.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">New product will be sent after
+                                    we receive your exchange product and confirm your return or exchange request.</span>
+                            </li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Maternal is not responsible
+                                    for lost items returned when sending back.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Initial shipping costs are
+                                    non-refundable.</span></li>
+                        </ul>
+                        <p><b>PRODUCT EXCHANGE POLICY</b></p>
+                        <ol>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Product can be exchanged when
+                                    it is damaged or defective, we do not accept exchange sizes.</span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">In the case that the desired
+                                    product is out of stock, the funds will be fully returned after being reduced by the
+                                    initial shipping cost. </span></li>
+                            <li style="font-weight: 400;"><span style="font-weight: 400;">Maternal is not responsible
+                                    for lost items returned when sending back.</span></li>
+                        </ol>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /#wrapper -->
