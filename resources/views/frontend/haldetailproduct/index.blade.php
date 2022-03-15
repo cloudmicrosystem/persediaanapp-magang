0608637227bcf9c22690fd550a4d60c9e18dd72e
dewa-wildan <wildanaryakusuma123@gmail.com>
2022-03-15 17:30:32 +0700

    pembenaran_haldetailproduct

--- resources/views/frontend/haldetailproduct/index.blade.php
+++ resources/views/frontend/haldetailproduct/index.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="{{ asset('assetcus/css/demo.css') }}" />
     <script src="{{ asset('assetcus/js/vendor/modernizr.js') }}"></script>
     <script src="{{ asset('assetcus/js/vendor/jquery.js') }}"></script>
+
     <!-- xzoom plugin here -->
     <script type="text/javascript" src="{{ asset('assetcus/js/xzoom.min.js') }}"></script>
     <link rel="stylesheet" type="text/css" href="{{ asset('assetcus/css/xzoom.css') }}" media="all" />
@@ -43,7 +44,7 @@
     <link rel="stylesheet" type="text/css"
         href="{{ asset('assetcus/vendors/daterangepicker/daterangepicker.css') }}">
     <!--===============================================================================================-->
-    <link rel="stylesheet" type="text/css" href="{{ asset('assetcus/vendors/slick/slick.css') }}">
+    {{-- <link rel="stylesheet" type="text/css" href="{{ asset('assetcus/vendors/slick/slick.css') }}"> --}}
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css" href="{{ asset('assetcus/vendors/lightbox2/css/lightbox.min.css') }}">
     <!--===============================================================================================-->
@@ -73,10 +74,165 @@
 <body class="animation">
 
     <!-- Header -->
-    @include('frontend.halcust.header')
+    <header>
+        <!-- Header desktop -->
+        <div class="wrap-menu-header color accent-dark ">
+            <div class="container h-full">
+                <div class="wrap_header trans-10 ">
+                    <!-- Logo -->
+                    <div class="logo">
+                        <a href="home">
+                            <img src="images/icons/logoa.png" alt="IMG-LOGO" data-logofixed="images/icons/logoa.png">
+                        </a>
+                    </div>
+
+                    <!-- Menu -->
+                    <div class="wrap_menu p-l-45 p-l-0-xl">
+                        <nav class="menu">
+                            <ul class="main_menu">
+                                <li>
+                                    <a href="/home">Home</a>
+                                </li>
+
+                                <li>
+                                    <a href="/product">Product</a>
+                                </li>
+
+                                <li>
+                                    <a href="/store">Store</a>
+                                </li>
+
+                                <li>
+                                    <a href="/artikel">Artikel</a>
+                                </li>
+
+                                <li>
+                                    <a href="/about">About</a>
+                                </li>
+
+                                <li>
+                                    <a href="/contact">Contact</a>
+                                </li>
+
+                                <li>
+                                    <a href="/contact.html">|</a>
+                                </li>
+
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <!-- Social -->
+                    <div class="social flex-w flex-l-m p-r-20">
+
+                        <a href="#"><i class="fa fa-user m-l-21" aria-hidden="true"></i></a>
+
+                        {{-- KERANJANG --}}
+                        {{-- <div class="span6">
+                            <div class="product-Info">
+                                <span class="keranjang_quantity"></span>
+                                <a href="#myModal" role="button" class="product-view" data-toggle="modal"><i
+                                        class="fa fa-shopping-cart m-l-21" aria-hidden="true"></i></a>
+                            </div>
+
+                            <div id="myModal" class="modal hide fade">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal"
+                                        aria-hidden="true">&times;</button>
+                                    <h3><span class="keranjang_quantity"></span> items</h3>
+                                </div>
+                                <div class="modal-body">
+                                    <div class="keranjang_items"></div>
+                                    <!-- Yep it's that simple -->
+                                    <hr>
+                                    <div class="left"><strong>Items: </strong><span
+                                            class="keranjang_quantity"></span></div>
+                                    <div class="right"><strong>Total: </strong><span
+                                            class="keranjang_total"></span></div>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="javascript:;" class="keranjang_empty">Empty</a>
+                                    <a href="javascript:;" class="keranjang_checkout">Checkout</a>
+                                </div>
+                            </div>
+
+
+
+                            <div class="product-total">
+
+                                <p><b>Items:</b> <span class="keranjang_quantity"></span></p>
+                                <!--number of items in the cart-->
+
+                                <p><b>Total:</b> <span class="keranjang_total"></span></p>
+                                <!-- total cost of cart before tax & shipping-->
+
+                                <p><b>Tax rate:</b> <span class="keranjang_taxRate"></span></p>
+                                <!--will be display as a percentage ( ie 7% )-->
+
+                                <p><b>Tax:</b> <span class="keranjang_tax"></span></p>
+                                <!--taxRate X total displayed as currency-->
+
+                                <p><b>Final price:</b> <span class="keranjang_grandTotal"></span> </p>
+                                <!--total of the cart after tax & shipping -->
+
+                            </div>
+                        </div><!-- /span6 --> --}}
+
+                        <a href="#"><i class="fa fa-shopping-cart m-l-21" aria-hidden="true"></i></a>
+
+                        <button class="btn-show-sidebar m-l-33 trans-0-4"></button>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </header>
 
     <!-- Sidebar -->
-    @include('frontend.halcust.sidebar')
+    <aside class="sidebar color accent-dark">
+        <!-- Button Hide sidebar -->
+        <button class="btn-hide-sidebar ti-close color accent-dark "></button>
+
+        <!-- - -->
+        <ul class="menu-sidebar p-t-95 p-b-70">
+            <li class="t-center m-b-13">
+                <a href="home" class="txt19">Home</a>
+            </li>
+
+            <li class="t-center m-b-13">
+                <a href="product" class="txt19">Product</a>
+            </li>
+
+            <li class="t-center m-b-13">
+                <a href="store" class="txt19">Store</a>
+            </li>
+
+            <li class="t-center m-b-13">
+                <a href="gallery.html" class="txt19">Artikel</a>
+            </li>
+
+            <li class="t-center m-b-13">
+                <a href="about" class="txt19">About</a>
+            </li>
+
+            <li class="t-center m-b-13">
+                <a href="blog.html" class="txt19">Blog</a>
+            </li>
+
+            <li class="t-center m-b-33">
+                <a href="contact.html" class="txt19">Contact</a>
+            </li>
+
+            <li class="t-center">
+                <!-- Button3 -->
+                <a href="reservation.html" class="btn3 flex-c-m size13 txt11 trans-0-4 m-l-r-auto">
+                    Reservation
+                </a>
+            </li>
+        </ul>
+
+    </aside>
+
 
     <!-- Slide1 -->
     {{-- <section class="section-slide">
